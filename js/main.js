9b16f095217d17ce0565199c3b2b54e75c55707c
Tara Whiteley <tarawhiteley@gmail.com>
2017-07-01 18:14:22 -0400

    tweak site, add animation to form

---
+++ js/main.js
@@ -0,0 +1,14 @@
+/*
+
+$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll >= 50) {
+      // put some documentation here
+      $('.js-nav').addClass('scroll-up');
+     } else {
+      $('.js-nav').removeClass('scroll-up');
+    }
+});
+
+*/
